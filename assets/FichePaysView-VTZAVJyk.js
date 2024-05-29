98dca260b969ac90f79cd71dcf4d54b55b3f2c5e
mbanais <anaismannee@gmail.com>
2024-05-28 23:06:33 -0400

    modification dist/ avec ajout hash router

--- assets/FichePaysView-VTZAVJyk.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c,a as s,F as o,o as t}from"./index-bwNCt_nB.js";const n={},r=s("p",null,"hello",-1),_={class:"infoPays"},i={class:"ctn_pays_drapeau"},l=["src"];function d(e,p){return t(),c(o,null,[r,s("div",_,[s("div",i,[s("img",{class:"drapeau",src:this.refImage+e.objPays[e.index].drapeau,width:"350px",alt:""},null,8,l)])])],64)}const u=a(n,[["render",d]]);export{u as default};
